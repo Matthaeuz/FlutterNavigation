b55ec9d6f74dac9835e28e86a369f3d3176dc641
Aloysius Beronque <aloysius.beronque@usjr.edu.ph>
2022-10-09 18:42:13 +0800

    PRELIMS

---
+++ lib/icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MyFlutterApp {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String? _kFontPkg = null;
+
+  static const IconData google = IconData(0xf1a0, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
